8acd5512ba3f381f5b28969740be52be7185c352
Justin Vititoe <jvit1985@yahoo.com>
2022-03-09 19:26:10 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,11 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
+var playerHealth = 10;
 var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
-
 
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -64,18 +62,49 @@ var fight = function(enemyName) {
     } // end of while loop
   }; // end of fight function 
 
-for (var i = 0; i < enemyNames.length; i++) {
+//function to start the game
+  var startGame = function() {
+     //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + (i+1));
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
-    debugger;
     fight(pickedEnemyName);
     }
     else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
     }
+  }
+  //function to end game
+  var endGame = function() {
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+      //restart the game
+      startGame();
+    }
+    else{
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+  //var playAgainConfirm = window.confirm("Would you like to play again?");
+  //if (playAgainConfirm) {
+    //restart the game
+   // startGame();
+ // }
+ // else{
+  //  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+  endGame();
 };
 
-//fight();
+startGame();
